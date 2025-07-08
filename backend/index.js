9f1d4a6bb8a1b112d8678728ac636788098408b9
fahim <fahimahamedweb@gmail.com>
2025-07-08 21:36:03 +0600

    some changes done

--- backend/index.js
+++ backend/index.js
@@ -9,13 +9,11 @@ dotenv.config();
 
 const app = express();
 
-// CORS setup (must come before routes)
-app.use(
-  cors({
-    origin: "http://localhost:5173", // Allow only your frontend
-    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
-  })
-);
+app.use(cors({
+    origin:['http://localhost:5173',''],
+    credentials:true   //  credentials releted joto jinish ase seta jate kaj kore example token
+  }))
+
 
 // Body parser (for JSON payloads)
 app.use(express.json({ limit: "25mb" }));
