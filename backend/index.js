ad5b554d7fd3da8e2aa801d2a1c14b4f81e983b3
fahim <fahimahamedweb@gmail.com>
2025-07-08 20:44:25 +0600

    db setup done

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,29 @@
 import express from "express"; 
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config()
 
 const app = express()
-
 const port = process.env.PORT || 5000;
+app.use(cors({
+    origin:['http://localhost:5173'],
+    credentials:true   //  credentials releted joto jinish ase seta jate kaj kore example token
+  })
+)
+app.use(express.json({ limit: "25mb" }));
+
+
+async function main() {
+    await mongoose.connect(process.env.DB_URL);
+      
+    }
+    main()
+    .then(() => console.log(" succesfully connected."))
+    .catch((err) => console.log(err));
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
 
 app.get("/", (req, res) => {
     res.send("Mini E-commerce Server is running....");
