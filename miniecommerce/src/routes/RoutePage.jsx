65b5a5abd21adf6458676a650c73b277c8dd0782
fahim <fahimahamedweb@gmail.com>
2025-07-09 07:05:22 +0600

    single product page added

--- miniecommerce/src/routes/RoutePage.jsx
+++ miniecommerce/src/routes/RoutePage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router";
 import App from "../App";
 import Home from "../pages/Home";
-
+import SingleProduct from "../pages/SingleProduct";
 
 const RoutePage = () => {
     return (
@@ -11,6 +11,7 @@ const RoutePage = () => {
           {/* Main App Routes */}
           <Route element={<App />}>
             <Route path="/" element={<Home />} />
+            <Route path="/shop/:id" element={<SingleProduct />} />
             </Route>
       </Routes>
     </div>
